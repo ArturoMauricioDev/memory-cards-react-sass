48c4dd288bd1d740e43bcb01edeb0ae75eb21151
Arturo <atercerosb@univalle.edu>
2021-07-10 16:15:40 -0400

    flipCard realizado

--- src/componentes/Card.js
+++ src/componentes/Card.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import FlipCard from 'react-flipcard';
+
 
 export default function Card(props) {
     return (
         <div className="Card">
-            <FlipCard>
-                <div className="portada"></div>
-                <div className="contenido">
+            <div className="Card-inner">
+                <div className="Card-front"></div>
+                <div className="Card-back">
                     <i className={`fa ${props.icono} fa-5x`} ></i>
                 </div>
-            </FlipCard>
-     
+            </div>
         </div>
     )
 }
